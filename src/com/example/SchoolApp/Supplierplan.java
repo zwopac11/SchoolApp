08e8c5ee0bfdf01cde0aaa807a3c343a3a0a203a
zwopac11 <zwopac11@htlkaindorf.at>
2014-10-08 14:04:18 +0200

    Start

--- src/com/example/SchoolApp/Supplierplan.java
+++ src/com/example/SchoolApp/Supplierplan.java
@@ -5,18 +5,20 @@ import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class Mail extends Activity {
+public class Supplierplan extends Activity {
 
     private WebView webView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState );
+
         setContentView(R.layout.mail);
 
         webView= (WebView) findViewById(R.id.webView);
 
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://login.microsoftonline.com/");
+        webView.loadUrl("https://supplierplan.htl-kaindorf.at/supp_neu/default.htm");
+        //https://supplierplan.htl-kaindorf.at/supp_neu/41/c/c00013.htm
     }
 }
